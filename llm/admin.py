493d70a6ecd6135c2c195f4daf3a54e7bec8c161
Michael <mikrlynch@gmail.com>
2023-03-26 17:21:21 -0400

    Add migration and register OpenAI model in admin

--- llm/admin.py
+++ llm/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from llm.models import ModelProviderAPI
 from llm.models import Prompt
+from llm.models.queries import OpenAIChatQuery
 
 # Register your models here.
 admin.site.register(ModelProviderAPI)
 admin.site.register(Prompt)
+admin.site.register(OpenAIChatQuery)
