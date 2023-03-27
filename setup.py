7f580b0e47c7034998fc94dd7778e262cb547bd5
Michael <mikrlynch@gmail.com>
2023-03-26 22:56:43 -0400

    Add license and update setup.py to use license

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="django-llm",
-    version="",
+    version="0.1.0",
     author="Michael Lynch",
     author_email="michael@flatlander.dev",
     description="A LLM (Large Language Model) app for Django",
     url="<package URL>",
-    license="<package license>",
+    license="MIT",
     packages=["django_llm"],
     install_requires=requirements,  # TODO test down to Django 3
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: <license name>",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
