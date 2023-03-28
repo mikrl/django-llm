e37e2ebfd4cda831f635d962454f211c61858b5b
Michael <mikrlynch@gmail.com>
2023-03-27 22:43:38 -0400

    Fix manager workflow

--- llm/managers/base.py
+++ llm/managers/base.py
@@ -1,10 +1,1 @@
-from django.db import models
-
-
-class PromptManager(models.Manager):
-    def to_json(self):
-        return {
-            "id": self.id,
-            "prompt": self.template,
-            "variables": self.get_input_variables(),
-        }
+# Manager Code Goes Here
--- llm/models/prompts.py
+++ llm/models/prompts.py
@@ -7,14 +7,22 @@ from llm.managers.base import PromptManager
 
 class Prompt(models.Model):
     template = models.TextField(_("Prompt Template"))
-    objects = PromptManager()
+    #objects = PromptManager()
 
     class Meta:
         verbose_name = "Prompt"
         verbose_name_plural = "Prompts"
 
-    def get_input_variables(self):
+    @property
+    def variables(self):
         # find all substrings within braces
         matches = re.findall(r"{([^}]*)}", self.template)
         # return list of unique matches
         return list(set(matches))
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "prompt": self.template,
+            "variables": self.variables,
+        }
