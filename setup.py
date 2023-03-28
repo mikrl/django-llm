46c816d1c75226a5b51de33a6ab1b52b0a581004
Michael <mikrlynch@gmail.com>
2023-03-28 11:40:33 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="django-llm",
-    version="0.1.2",
+    version="0.1.3",
     author="Michael Lynch",
     author_email="michael@flatlander.dev",
     description="A LLM (Large Language Model) app for Django",
